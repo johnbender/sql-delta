5df21dbf6654d9b705e22cfeb71fa6f0f5cdf466
John Bender <john.m.bender@gmail.com>
2013-09-06 23:37:19 -0700

    added compare typeclass to simplify things

--- src/Database/SqlDelta/Core.hs
+++ src/Database/SqlDelta/Core.hs
@@ -1,6 +1,7 @@
 module Database.SqlDelta.Core (reconcile) where
 
 import Database.HsSqlPpp.Ast (Statement(CreateTable),
+                              AttributeDef(..),
                               NameComponent(..),
                               nameComponents)
 
@@ -9,12 +10,29 @@ import Database.HsSqlPpp.Catalog ()
 import Data.Either (rights)
 import Data.Text.Lazy (pack)
 import Data.List (zip)
+import Text.Show.Pretty (ppShow)
+
+class (Eq a) => Diff a where
+    -- check for a difference
+    (<=>)  :: a -> a -> Bool
+    (<=>) = (==)
+
+    (</=>) :: a -> a -> Bool
+    (</=>) a b = not (a <=> b)
+
+instance Diff Statement where
+    (<=>) (CreateTable _ x1 y1 z1) (CreateTable _ x2 y2 z2) =
+        (x1, y1, z1) == (x2, y2, z2)
+
+instance Diff AttributeDef where
+    (<=>) (AttributeDef _ x1 y1 z1 xx1) (AttributeDef _ x2 y2 z2 xx2) =
+        (x1, y1, z1, xx1) == (x2, y2, z2, xx2)
 
 
 reconcile old new = do
   oldSql <- readFile old
   newSql <- readFile new
-  return $ zipWith diff (parse old oldSql) (parse new oldSql)
+  return $ ppShow $ zipWith diff (parse old oldSql) (parse new oldSql)
 
 parse path sql =
     let lines = Nothing
@@ -23,19 +41,21 @@ parse path sql =
          Right x -> x
          _       -> []
 
-diff old new | old == new = []
-             | otherwise  = diff' old new []
+diff old new | old <=> new  = []
+             | otherwise    = [new] --  new -- diff' old new []
 
 -- TODO handle constraints
-diff' old@(CreateTable _ oldName _ _) new@(CreateTable _ newName _ _) diffs =
-    if (name old) /= (name new) then
-        (show new):diffs
-    else
-        diffAttributes (attrs old) (attrs new) diffs
+diff' old@(CreateTable _ _ _ _) new@(CreateTable _ _ _ _) diffs =
+    diffAttributes (attrs old) (attrs new) diffs
 
-diff' (CreateTable _ _ _ _) x diffs = (show x):diffs
+diff' (CreateTable _ _ _ _) x diffs = ("", ""):diffs
 
-diffAttributes old new diffs = [ show y | (x, y) <- (zip old new), x /= y ] ++ diffs
+diffAttributes old new diffs =
+    let diffNames = [(attrName x, attrName y) | (x, y) <- zip old new ]
+    in diffNames ++ diffs
 
 attrs (CreateTable _ _ as _) = as
-name (CreateTable _ n  _ _) = show (map (\(Nmc s) -> s) (nameComponents n))
+
+tableName (CreateTable _ n _ _) = (map (\(Nmc s) -> s) (nameComponents n))
+
+attrName (AttributeDef _ (Nmc s) _ _ _) = s
--- src/Database/SqlDelta/Main.hs
+++ src/Database/SqlDelta/Main.hs
@@ -4,5 +4,4 @@ import Database.SqlDelta.Core (reconcile)
 main :: IO ()
 main = do
   [old, new] <- getArgs
-  result <- reconcile old new
-  print result
+  reconcile old new >>= putStrLn
