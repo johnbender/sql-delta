a2fd629003121231db21241f25e7258d27309f53
John Bender <john.m.bender@gmail.com>
2013-09-07 23:34:45 -0700

    attribute diff working

--- src/Database/SqlDelta/Core.hs
+++ src/Database/SqlDelta/Core.hs
@@ -9,30 +9,46 @@ import Database.HsSqlPpp.Parser (parseStatements, defaultParseFlags)
 import Database.HsSqlPpp.Catalog ()
 import Data.Either (rights)
 import Data.Text.Lazy (pack)
-import Data.List (zip)
+import Data.List (zip, foldl')
+import Data.Set (difference, fromList, toList)
 import Text.Show.Pretty (ppShow)
+import Debug.Trace (trace)
 
-class (Eq a) => Diff a where
+class (Ord a) => Diff a where
     -- check for a difference
-    (<=>)  :: a -> a -> Bool
-    (<=>) = (==)
+    (<=>) :: a -> a -> Bool
+    (<=>) x y = EQ == (compare x y)
 
     (</=>) :: a -> a -> Bool
-    (</=>) a b = not (a <=> b)
+    (</=>) x y = not (x <=> y)
 
-instance Diff Statement where
-    (<=>) (CreateTable _ x1 y1 z1) (CreateTable _ x2 y2 z2) =
-        (x1, y1, z1) == (x2, y2, z2)
+-- replacement for derived show instances, inteded to exclude anotation info
+class (Show a) => Shown a where
+    shown :: a -> String
+    shown = show
 
-instance Diff AttributeDef where
-    (<=>) (AttributeDef _ x1 y1 z1 xx1) (AttributeDef _ x2 y2 z2 xx2) =
-        (x1, y1, z1, xx1) == (x2, y2, z2, xx2)
+instance Shown Statement where
+    shown (CreateTable _ x attrs z) =
+        foldl' (\acc x -> acc ++ (shown x) ++ " ") "" attrs
 
+instance Shown AttributeDef where
+    shown (AttributeDef _ (Nmc name) _ _ _)  = name
+
+instance Ord Statement where
+    compare fst@(CreateTable _ _ _ _) snd@(CreateTable _ _ _ _) =
+        compare (shown fst) (shown snd)
+
+instance Ord AttributeDef where
+    compare fst@(AttributeDef _ _ _ _ _) snd@(AttributeDef _ _ _ _ _) =
+        compare (shown fst) (shown snd)
+
+instance Diff AttributeDef
+instance Diff Statement
 
 reconcile old new = do
   oldSql <- readFile old
   newSql <- readFile new
-  return $ ppShow $ zipWith diff (parse old oldSql) (parse new oldSql)
+  return $ ppShow $ zipWith diff (parse old oldSql) (parse new newSql)
 
 parse path sql =
     let lines = Nothing
@@ -41,18 +57,17 @@ parse path sql =
          Right x -> x
          _       -> []
 
-diff old new | old <=> new  = []
-             | otherwise    = [new] --  new -- diff' old new []
+diff old new | old <=> new = []
+             | otherwise    = diff' old new []
 
 -- TODO handle constraints
 diff' old@(CreateTable _ _ _ _) new@(CreateTable _ _ _ _) diffs =
     diffAttributes (attrs old) (attrs new) diffs
 
-diff' (CreateTable _ _ _ _) x diffs = ("", ""):diffs
+diff' (CreateTable _ _ as _) x diffs = as ++ diffs
 
 diffAttributes old new diffs =
-    let diffNames = [(attrName x, attrName y) | (x, y) <- zip old new ]
-    in diffNames ++ diffs
+    (toList $ difference (fromList old) (fromList new)) ++ diffs
 
 attrs (CreateTable _ _ as _) = as
 
